33e0ce4ef2bb7cd9ff87c6e4f992ddde01a000a7
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-02-10 20:59:39 -0300

    Adicionado o método toFixed para arredondamento em duas casas decimais

--- padaria_tech/js/scripts.js
+++ padaria_tech/js/scripts.js
@@ -1,5 +1,5 @@
 let unitPrice = 0.18;
 
 for(let i = 1; i <= 50; i++) {
-    console.log(`O preço de ${i} pães é R$ ${i * unitPrice} reais`);
+    console.log(`O preço de ${i} pães é R$ ${(i * unitPrice).toFixed(2)} reais`);
 }
