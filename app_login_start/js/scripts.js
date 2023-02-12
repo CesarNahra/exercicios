009e63aae4c04ac75fb3e4ff2f5d52469c7d0226
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-02-11 21:20:28 -0300

    Começo da lógica aplicação app start

--- app_login_start/js/scripts.js
+++ app_login_start/js/scripts.js
@@ -1,4 +1,33 @@
 const username = document.querySelector("#form-container #username");
 const password = document.querySelector("#form-container #pass");
+const btn = document.querySelector("#form-container .btn-submit");
 
-localStorage.setItem(username, password);
+
+function enviarForm(event) {
+}
+
+// const person = {
+//     username: "",
+//     password: "",
+//     setUser: function(newUser) {
+//         this.username = username;
+//     },
+//     setPass: function(newPass) {
+//         this.password = newPass;
+//     },
+//     getUser: function() {
+//         return this.username;
+//     },
+//     getPass: function() {
+//         return this.password;
+//     },
+// }
+
+const person = {
+    username: "",
+    password: "",
+}
+
+let pessoaTexto = JSON.stringify(person);
+
+console.log(pessoaTexto);
