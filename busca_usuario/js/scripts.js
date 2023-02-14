c28881034f85c37c66a640a11cb5f9ca4a629eec
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-02-14 13:30:39 -0300

    Aplicação da busca de usuário feita

---
+++ busca_usuario/js/scripts.js
@@ -0,0 +1,34 @@
+const users = [
+    {
+        name: "Scenic Stream",
+        username: "Eladio66",
+    },
+    {
+        name: "Indigo Shady Stream",
+        username:  "Marian_Volkman10",
+    },
+    {
+        name: "Historic Trail",
+        username: "Tabitha_Bosco",
+    },
+    {
+        name: "Old Footpath",
+        username: "Ford50",
+    },
+    {
+        name: "Viovar Historic Route",
+        username: "Soledad_Olson",
+    },
+    {
+        name: "Orange Shady Footpath",
+        username: "Jarvis_Hackett",
+    },
+    {
+        name: "Blue Stream",
+        username: "Blue Stream",
+    }
+]
+
+for(let i = 0; i < users.length; i++) {
+    console.log(`O usuário ${users[i].name} é dono do username ${users[i].username}`);
+}
