e197d09cc7d3d7c0196a9002cb21a2d8179a61ca
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-02-18 11:20:43 -0300

    Início do DOM

--- rick_and_morty/js/scripts.js
+++ rick_and_morty/js/scripts.js
@@ -1,3 +1,12 @@
+let charName = document.querySelector(".name");
+let charPhoto = document.querySelector(".photo");
+let charSituation = document.querySelector(".alive");
+let charCn = document.querySelector(".cityname");
+let charOn = document.querySelector(".originname");
+let charGender = document.querySelector(".type");
+let charSpecie = document.querySelector(".human");
+
+
 let url = "https://rickandmortyapi.com/api/character/1"
 
 async function search() {
@@ -5,5 +14,7 @@ async function search() {
     const data =  await response.json();
     console.log(data);
 }
+
+charGender.innerHTML = "oi";
 
 search();
