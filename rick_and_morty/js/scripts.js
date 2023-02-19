9b5df9e72e8f22411b69417633d092e4dc88fd59
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-02-18 22:31:51 -0300

    100% Rick and Morty

--- rick_and_morty/js/scripts.js
+++ rick_and_morty/js/scripts.js
@@ -12,7 +12,9 @@ let url = "https://rickandmortyapi.com/api/character/1"
 async function search() {
     const response = await fetch(url);
     const data =  await response.json();
+    console.log(data);
     charName.innerHTML = data.name;
+    charPhoto.innerHTML = `<img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg">`
     charSituation.innerHTML = data.status;
     charCity.innerHTML = data.location.name;
     charOrigin.innerHTML = data.origin.name;
