6937d6c90bf84ed23f0ff2dfdc5f0a04eadbcca9
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-02-18 22:42:39 -0300

    100% DEV Profile

--- dev_profile/js/scripts.js
+++ dev_profile/js/scripts.js
@@ -8,6 +8,7 @@ async function search() {
     const response = await fetch(url);
     const data =  await response.json();
     myName.innerHTML = data.login;
+    avatar.innerHTML = `<img src="https://avatars.githubusercontent.com/u/105174608?v=4">`
 }
 
 search();
