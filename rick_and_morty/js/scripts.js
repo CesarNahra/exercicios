5080818a277904d1d7a1a9622669681ef288bb08
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-02-18 22:21:56 -0300

    Conclusão app Rick and Morty

--- rick_and_morty/js/scripts.js
+++ rick_and_morty/js/scripts.js
@@ -12,7 +12,6 @@ let url = "https://rickandmortyapi.com/api/character/1"
 async function search() {
     const response = await fetch(url);
     const data =  await response.json();
-    console.log(data);
     charName.innerHTML = data.name;
     charSituation.innerHTML = data.status;
     charCity.innerHTML = data.location.name;
