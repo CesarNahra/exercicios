538ab2b72f986b7955974cf9645a11ae24c2effa
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-02-19 11:33:23 -0300

    Adição das cores oficiais do desenho

--- rick_and_morty/js/scripts.js
+++ rick_and_morty/js/scripts.js
@@ -7,14 +7,14 @@ const charGender = document.querySelector(".type");
 const charSpecie = document.querySelector(".human");
 
 
-let url = "https://rickandmortyapi.com/api/character/1"
+let url = "https://rickandmortyapi.com/api/character/70"
 
 async function search() {
     const response = await fetch(url);
     const data =  await response.json();
     console.log(data);
     charName.innerHTML = data.name;
-    charPhoto.innerHTML = `<img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg">`
+    charPhoto.innerHTML = `<img src="https://rickandmortyapi.com/api/character/avatar/70.jpeg">`
     charSituation.innerHTML = data.status;
     charCity.innerHTML = data.location.name;
     charOrigin.innerHTML = data.origin.name;
