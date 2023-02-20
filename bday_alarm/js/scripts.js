11a81a97a51cf39b36ee2b5c6d55e92f54249052
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-02-20 11:11:16 -0300

    Correção na lógica da diferença

--- bday_alarm/js/scripts.js
+++ bday_alarm/js/scripts.js
@@ -1,9 +1,9 @@
-const aniversario = new Date(2000, 4, 8);
+const aniversario = new Date(2023, 4, 8);
 
 const hoje = new Date();
 
 if(aniversario === hoje) {
     console.log("Feliz Aniversário");
 } else {
-    console.log(`Faltam ${(hoje - aniversario)} dias`);
+    console.log(`Faltam ${(aniversario - hoje)} dias`);
 }
