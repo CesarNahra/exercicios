893939bec148af58a1267326d2f46c1e235956b8
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-02-22 21:23:52 -0300

    Finalizando banco poupancito

--- banco_poupancito/js/scripts.js
+++ banco_poupancito/js/scripts.js
@@ -27,3 +27,21 @@ class PoupançaPremium extends ContaPoupanca {
         this.juros = 1.2;
     }
 }
+
+let dependente2 = new PoupançaPremium(10000, 1);
+
+dependente2.atualizaJuros();
+
+console.log(dependente2);
+
+let dependente = new ContaPoupanca(10000, 1);
+
+console.log(dependente);
+
+dependente.atualizaJuros();
+
+console.log(dependente);
+
+let conta = new Conta(10000, 0);
+
+console.log(conta);
