bbd2a4b159924da2bae21c31fda268c52aa4547a
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-02-28 20:09:34 -0300

    Concluído lista

--- lista/js/scripts.js
+++ lista/js/scripts.js
@@ -3,6 +3,10 @@ function trocarPosicao(inicial, final) {
 
     const novaLista = [];
 
+    if(inicial < 0 || final < 0) {
+        console.log("Não pode enviar");
+    }
+
     for(let i = 0; i < lista.length; i++) {
         if(i === inicial) {
             novaLista.push(lista[final]);
@@ -18,4 +22,4 @@ function trocarPosicao(inicial, final) {
     console.log(novaLista);
 }
 
-trocarPosicao(0, 2);
+trocarPosicao(-1, 2);
