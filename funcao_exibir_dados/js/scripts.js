630b95ebe78040444a5366092795cc4f5ebc9ef8
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-03-02 20:14:41 -0300

    exercicio exibir dados completo, mas necessita refatoração

---
+++ funcao_exibir_dados/js/scripts.js
@@ -0,0 +1,11 @@
+function showData() {
+    let arrNomes = ['Pedro', 'José', 'Aderbal', 'Danilo', 'Luisa', 'Vitoria'];
+
+    let arrFrutas = ["Banana", "Morango", "Maçã", "Uva", "Pera", "Laranja"];
+
+    let arrayConcatenado = arrNomes.concat(arrFrutas);
+
+    console.log(arrayConcatenado);
+}
+
+showData();
