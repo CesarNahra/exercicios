836a4810672f9545d0018f1f13d0899108f4d3ac
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-03-05 19:37:50 -0300

    Conclusão exercício exibir dados

--- funcao_exibir_dados/js/scripts.js
+++ funcao_exibir_dados/js/scripts.js
@@ -3,9 +3,16 @@ function showData() {
 
     let arrFrutas = ["Banana", "Morango", "Maçã", "Uva", "Pera", "Laranja"];
 
-    let arrayConcatenado = arrNomes.concat(arrFrutas);
+    let arrFrutasInv = arrFrutas.reverse();
 
-    console.log(arrayConcatenado);
+    let arrFormado = [];
+
+    for(let i = 0; i < arrNomes.length && arrFrutas.length; i++) {
+        console.log(`${arrNomes[i]} - ${arrFrutasInv[i]}`);
+        arrFormado.push(`${arrNomes[i]} - ${arrFrutasInv[i]}`);
+    }
+
+    console.log(arrFormado);
 }
 
 showData();
